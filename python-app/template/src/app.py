9b726aecbb42ac2c49651d75bfb32697d6ebf199
James Tandy <james.tandy@gmail.com>
2026-07-03 20:28:37 +1000

    Add python app software template

---
+++ python-app/template/src/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def health_check():
+    return {"status": "healthy"}, 200
+
+@app.route('/api/v1/info')
+
+def info():
+    return jsonify({
+        'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'hostname': socket.gethostname(),
+        'message': 'This is the details page!',
+        'deployed_on': 'kubernetes',
+        'env': '${{ values.app_env }}',
+        'app_name': '${{ values.app_name }}'
+    })
+
+@app.route('/api/v1/healthz')
+
+def healthz():
+    return jsonify({'status': 'up'}), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
+
+# '/api/v1/details'
+# '/api/v1/healthz'
